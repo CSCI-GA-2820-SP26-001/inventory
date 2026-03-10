8db675f19b040be6d1e8b41e11cdd43e29de9a24
juanpablojaramilloarias <jj4383@gmail.com>
2026-03-10 00:01:06 +0000

    create_inv_testing_in_factory

--- tests/factories.py
+++ tests/factories.py
@@ -3,18 +3,25 @@ Test Factory to make fake objects for testing
 """
 
 import factory
-from service.models import YourResourceModel
+from factory.fuzzy import FuzzyChoice, FuzzyInteger
+from datetime import datetime
+
+from service.models import Inventory, ItemCondition
 
 
-class YourResourceModelFactory(factory.Factory):
-    """Creates fake pets that you don't have to feed"""
+class InventoryFactory(factory.Factory):
+    """Factory for creating Inventory model instances for tests"""
 
     class Meta:  # pylint: disable=too-few-public-methods
-        """Maps factory to data model"""
-
-        model = YourResourceModel
+        model = Inventory
 
     id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
-
-    # Todo: Add your other attributes here...
+    name = factory.Faker("word")
+    product_id = factory.Faker("uuid4")
+    quantity_on_hand = FuzzyInteger(0, 1000)
+    restock_level = FuzzyInteger(0, 200)
+    condition = FuzzyChoice(
+        choices=[ItemCondition.NEW, ItemCondition.OPEN_BOX, ItemCondition.USED]
+    )
+    created_at = factory.LazyFunction(datetime.utcnow)
+    last_updated = factory.LazyFunction(datetime.utcnow)
