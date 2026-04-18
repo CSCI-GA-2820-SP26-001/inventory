28af9afa118c6578f2f98e35864a680ca37b1bec
Prajanya Gupta <65920698+Prajanya-g@users.noreply.github.com>
2026-04-18 18:00:10 -0400

    implemented health endpoint

--- service/routes.py
+++ service/routes.py
@@ -55,6 +55,12 @@ def favicon():
     return "", status.HTTP_204_NO_CONTENT
 
 
+@app.route("/health", methods=["GET"])
+def health():
+    """Health check for load balancers and orchestrators."""
+    return jsonify({"status": "OK"}), status.HTTP_200_OK
+
+
 ######################################################################
 #  R E S T   A P I   E N D P O I N T S
 ######################################################################
